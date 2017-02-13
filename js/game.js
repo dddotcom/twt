8f5f09d20ef1b5b264173a4478bbfd22280951f7
dddotcom <dddotcom@gmail.com>
2017-02-12 22:55:45 -0800

    added actionTimer which is the number of ms the action must be held for before obtaining points; added keyCapture so that action keys have the duration property

--- js/game.js
+++ js/game.js
@@ -10,6 +10,7 @@ var gameProperties = {
   itemMaxWidth: 800,
   bathroomBgName: 'bathroom',
   bathroomBgURL: 'assets/bathroom1.png',
+  actionTimer: 500,
 }
 
 var players = [
@@ -141,8 +142,8 @@ mainState.prototype = {
     var actionIndex = items.indexOf(item.key);
     var actionPoints = actions[actionIndex].points;
     var playersIndex = player.key.split("player")[1];
-
-    if(players[playersIndex].actionListeners[actionIndex].isDown){
+    var actionKey = players[playersIndex].actionListeners[actionIndex];
+    if(actionKey.isDown && actionKey.duration == gameProperties.actionTimer){
       item.kill();
       players[playersIndex].score += actionPoints;
       this.showPoints(player, actionPoints);
@@ -231,8 +232,10 @@ mainState.prototype = {
     for(var i = 0; i < players.length; i++){
       for(var y = 0; y < actions.length; y++){
         players[i].actionListeners.push(game.input.keyboard.addKey(actions[y].command[i]));
+        game.input.keyboard.addKeyCapture(actions[y].command[i]);
       }
     }
+
 
     //only enable those that are relevant to the level
     this.enableRelevantActions();
